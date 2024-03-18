2399434c470cdf75cfd554286cb6ab727807ef7f
Trof_r <trofimenkork08@gmail.com>
2024-03-18 18:15:46 +0300

    update answers form

--- src/components/AnswerForm.vue
+++ src/components/AnswerForm.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="answer-form p-4" ref="root">
         <h2 class="block-title mb-4">Форма для заполнения</h2>
-        <iframe src="https://forms.yandex.ru/cloud/65f6d951c417f30fec60e9bb/?iframe=1" frameborder="0" name="ya-form-65f6d951c417f30fec60e9bb" width="100%" height="100%"></iframe>
+        <iframe src="https://forms.yandex.ru/cloud/65f8577c84227c1e89492202/?iframe=1" frameborder="0" name="ya-form-65f6d951c417f30fec60e9bb" width="100%" height="100%"></iframe>
     </div>
 </template>
 
